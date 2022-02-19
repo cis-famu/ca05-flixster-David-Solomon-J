758485d718ed269a252a46a666fd6db7a2fbbb05
David-Solomon-J <44678769+David-Solomon-J@users.noreply.github.com>
2022-02-18 19:52:51 -0500

    D.S.

---
+++ flixster/src/components/MovieCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Movielist from "./Movielist";
+
+//When your not making API calls you can make it sateless, juh a function
+function MovieCard(props) {
+    let movie = props.movie;
+    const altSrc = "https://image.tmdb.org/t/p/original/" + movie.poster_path;
+    const altText = movie.original_title + " image.";
+
+    return (
+
+        <div className="col mb-2" >
+            <div className="card h-100 position-relative">
+                <img className="card-img-top book-mg mx-auto pt-1" src={altSrc} className="card-img-top book-img mx-auto pt-1" alt={altText}/>
+                <div className="card-body book-card-details">
+                    <h5 className="card-title movie-title">{movie.original_title}</h5>
+                    <p className="card-text">{movie.overview}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MovieCard;
---
+++ flixster/src/components/Movielist.js
@@ -0,0 +1,59 @@
+//To use file as component must import react component
+import React, {Component} from 'react';
+import axios from "axios";
+import MovieCard from "./MovieCard";
+
+class Movielist extends Component {
+    //Creates a state variable
+    constructor(props) {
+        super(props);
+        this.state = { movies: [] };
+
+    }
+
+    componentDidMount() {
+        //Creates a variable to grab the url of the database with our api key
+        const MOVIES_DB_URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed`;
+        const getMovies = async () => {
+
+            //Way of getting data over the internet
+            await axios.get(MOVIES_DB_URL)
+                .then(response =>{
+                    console.log(response);
+                    //set state variable, if unsure what the data looks like console log it
+                    this.setState({movies: response.data.results});
+                }).catch((err) =>{
+                    console.log("Fetch Error: " + err)
+                });
+        }
+
+        getMovies();
+    }
+
+    render() {
+
+        return (
+            <div className="container">
+                <div className="row row-cols-sm-2 row-cols-md-5 row-cols-xxl-4">
+                    {
+                        //arrow means function
+                        //Map function always require return
+                        this.state.movies.map(movie => {
+
+                            return (
+
+                                <MovieCard  movie={movie} />
+
+                            );
+
+                        })
+
+                    }
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Movielist;
