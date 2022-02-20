cc6c0e172119c11f448a4f3d58aa3fafe0697daf
David-Solomon-J <44678769+David-Solomon-J@users.noreply.github.com>
2022-02-19 23:44:27 -0500

    D.S.

--- flixster/src/App.js
+++ flixster/src/App.js
@@ -4,13 +4,16 @@ import './App.css';
 function App() {
   return (
       <>
-          <div className="container">
+
+          <div className="container" >
+              <body>
               <div className="row">
                   <div className="col-12">
-                      <h3 className="display-4 page-title text-center m-3">Now Playing Movies</h3>
-                      <hr/>
+                      <h3 className="display-4 page-title text-center m-3">Today's Films</h3>
+                      <br/>
                   </div>
               </div>
+              </body>
           </div>
 
           <Movielist />
